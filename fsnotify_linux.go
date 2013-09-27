63f9f988eb3c4aa3c1a4d4d647ec204c34436cb2
Nathan Youngman <git@nathany.com>
2013-09-26 19:00:24 -0600

    remove FD_SET and friends
    should not have been exported
    
    https://github.com/howeyc/fsnotify/issues/64#issuecomment-24915706

--- fsnotify_linux.go
+++ fsnotify_linux.go
@@ -300,17 +300,3 @@ func (e *FileEvent) ignoreLinux() bool {
 	}
 	return false
 }
-
-func FD_SET(p *syscall.FdSet, i int) {
-	p.Bits[i/64] |= 1 << (uint(i) % 64)
-}
-
-func FD_ISSET(p *syscall.FdSet, i int) bool {
-	return (p.Bits[i/64] & (1 << (uint(i) % 64))) != 0
-}
-
-func FD_ZERO(p *syscall.FdSet) {
-	for i := range p.Bits {
-		p.Bits[i] = 0
-	}
-}
