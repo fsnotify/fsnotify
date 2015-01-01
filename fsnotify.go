b60ebf7604dd4945d4d891f3c752f528b433d135
Nathan Youngman <git@nathany.com>
2014-12-31 19:55:24 -0700

    update package description

--- fsnotify.go
+++ fsnotify.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package fsnotify implements file system notification.
+// Package fsnotify provides a platform-independent interface for file system notifications.
 package fsnotify
 
 import "fmt"
