b63213bcb386d7037336c0c33165dfe4da8d661c
Dan Miller <dmiller@windmill.engineering>
2018-05-25 16:15:38 -0400

    Fix debugging on macos

--- kqueue.go
+++ kqueue.go
@@ -517,6 +517,14 @@ func register(kq int, fds []int, flags int, fflags uint32) error {
 	return nil
 }
 
+func min(x int, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
 // read retrieves pending events, or waits until an event occurs.
 // A timeout of nil blocks indefinitely, while 0 polls the queue.
 func read(kq int, events []unix.Kevent_t, timeout *unix.Timespec) ([]unix.Kevent_t, error) {
@@ -524,6 +532,8 @@ func read(kq int, events []unix.Kevent_t, timeout *unix.Timespec) ([]unix.Kevent
 	if err != nil {
 		return nil, err
 	}
+	// https://github.com/fsnotify/fsnotify/issues/212#issuecomment-360862770
+	n = min(n, len(events)-1)
 	return events[0:n], nil
 }
 
