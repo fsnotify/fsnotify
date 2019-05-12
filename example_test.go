f100c507228c138ad54f3c819903ad714c507681
dev_auto <mcauto@naver.com>
2019-05-12 19:35:30 +0900

    modify example code

--- example_test.go
+++ example_test.go
@@ -8,41 +8,51 @@ package fsnotify_test
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-func ExampleNewWatcher() {
+func exampleNewWatcher(t *testing.T) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		t.Error(err)
 	}
 	defer watcher.Close()
 
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error:", err)
+	err = watcher.Add(".")
+	if err != nil {
+		t.Error(err)
+	}
+
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+MAIN:
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok { // when close channel ex) close(watcher.Events)
+				break MAIN
+			}
+			log.Println("event:", event)
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				log.Println("modified file:", event.Name)
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok { // when close channel ex) close(watcher.Errors)
+				break MAIN
+			}
+			log.Println("error:", err)
+
+		case signal := <-sigs:
+			if signal == os.Interrupt {
+				break MAIN
+				// close(watcher.Events) or close(watcher.Errors)
 			}
 		}
-	}()
-
-	err = watcher.Add("/tmp/foo")
-	if err != nil {
-		log.Fatal(err)
 	}
-	<-done
 }
