4caa2370cac22da7385cf92db55feeca7481068f
Martin Tournoij <martin@arp242.net>
2022-10-14 17:49:13 +0200

    Add failing test for #277

--- backend_kqueue.go
+++ backend_kqueue.go
@@ -368,7 +368,6 @@ const noteAllEvents = unix.NOTE_DELETE | unix.NOTE_WRITE | unix.NOTE_ATTRIB | un
 // Returns the real path to the file which was added, if any, which may be different from the one passed in the case of symlinks.
 func (w *Watcher) addWatch(name string, flags uint32) (string, error) {
 	var isDir bool
-	// Make ./name and name equivalent
 	name = filepath.Clean(name)
 
 	w.mu.Lock()
