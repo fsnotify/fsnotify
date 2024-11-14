fsnotify/fsnotify

commit e75779f82599782de2a8a0fb429351df3a773771

inotify: don't send event for IN_DELETE_SELF when also watching the parent (#620)
This would result in two events being sent.

Fixes #299

backend_inotify.go
@@ -565,16 +565,24 @@ func (w *Watcher) readEvents() {
 				}
 			}
 
-			event := w.newEvent(name, mask)
+			skip := mask&unix.IN_IGNORED != 0
 
-			// Send the events that are not ignored on the events channel
-			if mask&unix.IN_IGNORED == 0 {
-				if !w.sendEvent(event) {
+			/// Skip if we're watching both this path and the parent; the parent
+			/// will already send a delete so no need to do it twice.
+			if !skip && mask&unix.IN_DELETE_SELF != 0 {
+				if _, ok := w.watches.path[filepath.Dir(watch.path)]; ok {
+					skip = true
+				}
+			}
+
+			/// Send the events that are not ignored on the events channel
+			if !skip {
+				if !w.sendEvent(w.newEvent(name, mask)) {
 					return
 				}
 			}
 
-			// Move to the next event in the buffer
+			/// Move to the next event in the buffer
 			offset += unix.SizeofInotifyEvent + nameLen
 		}
 	}

fsnotify_test.go
@@ -720,6 +720,41 @@ func TestWatchRemove(t *testing.T) {
 			write /dir2/subdir
 			write /dir2/subdir/file
 		`},
+
+		{"remove while also watching parent", func(t *testing.T, w *Watcher, tmp string) {
+			// TODO: kind of a mess on Windows; sends something along the lines
+			// of:
+			//
+			//   create    /abc
+			//   write     /abc
+			//   write     /abc/def
+			//   write     /abc/def
+			//   remove    /abc/def
+			//   write     /abc
+			//   remove    /abc
+			//   remove    /abc
+			//
+			// But sometimes there's one or two more or less.
+			if runtime.GOOS == "windows" {
+				t.Skip("flaky")
+			}
+			addWatch(t, w, tmp)
+			mkdirAll(t, tmp, "/abc/def/ghi")
+			addWatch(t, w, tmp, "/abc")
+			rmAll(t, tmp, "/abc")
+		}, `
+			create    /abc
+			remove    /abc/def
+			remove    /abc
+
+			# TODO: no remove events?
+			dragonfly:
+				create    /abc
+			# TODO: no event for /abc/def?
+			fen:
+				create    /abc
+				remove    /abc
+		`},
 	}
 
 	for _, tt := range tests {

commit 80e756c78758dc06586e0ce79b49dbd0a0e3ed41

inotify: fix potential data race on IN_DELETE_SELF event
The blamed commit introduced a check to prevent sending a delete event
in case the parent is being watched as well. However, it accesses the
`watches.path` map without synchronization, possibly leading to a data
race in case the same map is accessed in parallel (e.g., by an `Add()`
operation). Let's fix this by grabbing the associated lock before
reading from the map.

Excerpt of the data race trace:

Write at 0x00c0004d8cf0 by goroutine 33:
  runtime.mapassign_faststr()
      golang.org/toolchain@v0.0.1-go1.23.0.linux-amd64/src/runtime/map_faststr.go:223 +0x0
  github.com/fsnotify/fsnotify.(*watches).updatePath()
      github.com/fsnotify/fsnotify/backend_inotify.go:163 +0x2b8
  github.com/fsnotify/fsnotify.(*inotify).register()
      github.com/fsnotify/fsnotify/backend_inotify.go:335 +0x1bd
  github.com/fsnotify/fsnotify.(*inotify).add()
      github.com/fsnotify/fsnotify/backend_inotify.go:331 +0xee
  github.com/fsnotify/fsnotify.(*inotify).AddWith()
      github.com/fsnotify/fsnotify/backend_inotify.go:296 +0x405
  github.com/fsnotify/fsnotify.(*inotify).Add()
      github.com/fsnotify/fsnotify/backend_inotify.go:253 +0x44
  github.com/fsnotify/fsnotify.(*Watcher).Add()
      github.com/fsnotify/fsnotify/fsnotify.go:313 +0x5ce

Previous read at 0x00c0004d8cf0 by goroutine 32:
  runtime.mapaccess2_faststr()
      golang.org/toolchain@v0.0.1-go1.23.0.linux-amd64/src/runtime/map_faststr.go:117 +0x0
  github.com/fsnotify/fsnotify.(*inotify).readEvents()
      github.com/fsnotify/fsnotify/backend_inotify.go:534 +0xd04
  github.com/fsnotify/fsnotify.newBufferedBackend.gowrap1()
      github.com/fsnotify/fsnotify/backend_inotify.go:195 +0x33

Fixes: e75779f82599 ("inotify: don't send event for IN_DELETE_SELF when also watching the parent (#620)")

backend_inotify.go
@@ -531,7 +531,11 @@ func (w *inotify) readEvents() {
 			/// Skip if we're watching both this path and the parent; the parent
 			/// will already send a delete so no need to do it twice.
 			if mask&unix.IN_DELETE_SELF != 0 {
-				if _, ok := w.watches.path[filepath.Dir(watch.path)]; ok {
+				w.watches.mu.RLock()
+				_, ok := w.watches.path[filepath.Dir(watch.path)]
+				w.watches.mu.RUnlock()
+
+				if ok {
 					next()
 					continue
 				}