fsnotify/fsnotify

commit d2ee00e6a4a4a31361154ea366272f0c23fc881c

kqueue: emit events as "/path/dir/file" instead of "path/link/file" (#625)
When watching a symlink to a directory it would emit events with the
target name, rather than the link name.

backend_kqueue.go
@@ -145,6 +145,7 @@ type (
 	watch struct {
 		wd       int
 		name     string
+		linkName string // In case of links; name is the target, and this is the link.
 		isDir    bool
 		dirFlags uint32
 	}
@@ -208,12 +209,12 @@ func (w *watches) addLink(path string, fd int) {
 	w.seen[path] = struct{}{}
 }
 
-func (w *watches) add(path string, fd int, isDir bool) {
+func (w *watches) add(path, linkPath string, fd int, isDir bool) {
 	w.mu.Lock()
 	defer w.mu.Unlock()
 
 	w.path[path] = fd
-	w.wd[fd] = watch{wd: fd, name: path, isDir: isDir}
+	w.wd[fd] = watch{wd: fd, name: path, linkName: linkPath, isDir: isDir}
 
 	parent := filepath.Dir(path)
 	byDir, ok := w.byDir[parent]
@@ -569,6 +570,7 @@ func (w *Watcher) addWatch(name string, flags uint32) (string, error) {
 				return link, nil
 			}
 
+			info.linkName = name
 			name = link
 			fi, err = os.Lstat(name)
 			if err != nil {
@@ -600,7 +602,7 @@ func (w *Watcher) addWatch(name string, flags uint32) (string, error) {
 	}
 
 	if !alreadyWatching {
-		w.watches.add(name, info.wd, info.isDir)
+		w.watches.add(name, info.linkName, info.wd, info.isDir)
 	}
 
 	// Watch the directory if it has not been watched before, or if it was
@@ -678,7 +680,7 @@ func (w *Watcher) readEvents() {
 				continue
 			}
 
-			event := w.newEvent(path.name, mask)
+			event := w.newEvent(path.name, path.linkName, mask)
 
 			if event.Has(Rename) || event.Has(Remove) {
 				w.remove(event.Name, false)
@@ -733,8 +735,14 @@ func (w *Watcher) readEvents() {
 }
 
 // newEvent returns an platform-independent Event based on kqueue Fflags.
-func (w *Watcher) newEvent(name string, mask uint32) Event {
+func (w *Watcher) newEvent(name, linkName string, mask uint32) Event {
 	e := Event{Name: name}
+	if linkName != "" {
+		// If the user watched "/path/link" then emit events as "/path/link"
+		// rather than "/path/target".
+		e.Name = linkName
+	}
+
 	if mask&unix.NOTE_DELETE == unix.NOTE_DELETE {
 		e.Op |= Remove
 	}

testdata/watch-symlink/to-dir
@@ -9,6 +9,3 @@ touch /dir/file
 
 Output:
 	create    /link/file
-
-	kqueue:  # TODO: Should use the link name.
-		create /dir/file

testdata/watch-symlink/to-file
@@ -10,7 +10,5 @@ echo hello >>/file
 Output:
 	write    /link
 
-	kqueue:   # TODO: Should use the link name.
-		write    /file
 	windows: # TODO: investigate.
 		no-events

commit 3dec80b9a4543dc04194d0557c384492d627fc3b

Correctly mark pre-existing entries when watching a link to a dir on kqueue
It would mark /dir/existing as seen, but would check for /link/existing.
So on the first change in a directory it would list all pre-existing
entries in the directory.

Regression from d2ee00e; just wasn't a testcase.

backend_kqueue.go
@@ -450,7 +450,11 @@ func (w *kqueue) addWatch(name string, flags uint32, listDir bool) (string, erro
 		}
 
 		if watchDir {
-			if err := w.watchDirectoryFiles(name); err != nil {
+			d := name
+			if info.linkName != "" {
+				d = info.linkName
+			}
+			if err := w.watchDirectoryFiles(d); err != nil {
 				return "", err
 			}
 		}

testdata/watch-symlink/to-dir
@@ -2,9 +2,10 @@
 require symlink
 
 mkdir /dir
+touch /dir/existing
 ln -s /dir /link
-watch /link
 
+watch /link
 touch /dir/file
 
 Output: