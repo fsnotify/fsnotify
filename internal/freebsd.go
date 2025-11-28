56d6a2310697a9319e5c2cffc9c5ca23ddf64fa5
Martin Tournoij <martin@arp242.net>
2025-11-28 03:58:18 +0000

    Remove internal.Maxfiles()
    Should have been removed with 73eaa1b

--- internal/freebsd.go
+++ internal/freebsd.go
@@ -13,8 +13,5 @@ var (
 	ErrUnixEACCES    = unix.EACCES
 )
 
-var maxfiles uint64
-
-func Maxfiles() uint64                              { return maxfiles }
 func Mkfifo(path string, mode uint32) error         { return unix.Mkfifo(path, mode) }
 func Mknod(path string, mode uint32, dev int) error { return unix.Mknod(path, mode, uint64(dev)) }
