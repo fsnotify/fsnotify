fsnotify/fsnotify

commit 3ec051739a75bb660d09c1b6eb2e01e8c868468a

Fix relative links on kqueue
Also only resolve symlinks explicitly given in Add()/AddWith(), and not
those added "internally" as part of a listdir. Also don't ignore errors
from os.Readlink() and os.Lstat; this was added in 2012 with bf36090 to
ignore unresolvable symlinks when listing a directory, but we're no
longer calling lstat() on the result of a link unless it's explicitly
passed with Add().

Fixes #661

backend_kqueue.go
@@ -305,7 +305,7 @@ func (w *kqueue) AddWith(name string, opts ...addOpt) error {
 		return fmt.Errorf("%w: %s", xErrUnsupported, with.op)
 	}
 
-	_, err := w.addWatch(name, noteAllEvents)
+	_, err := w.addWatch(name, noteAllEvents, false)
 	if err != nil {
 		return err
 	}
@@ -368,7 +368,7 @@ const noteAllEvents = unix.NOTE_DELETE | unix.NOTE_WRITE | unix.NOTE_ATTRIB | un
 // described in kevent(2).
 //
 // Returns the real path to the file which was added, with symlinks resolved.
-func (w *kqueue) addWatch(name string, flags uint32) (string, error) {
+func (w *kqueue) addWatch(name string, flags uint32, listDir bool) (string, error) {
 	if w.isClosed() {
 		return "", ErrClosed
 	}
@@ -387,15 +387,15 @@ func (w *kqueue) addWatch(name string, flags uint32) (string, error) {
 			return "", nil
 		}
 
-		// Follow symlinks.
-		if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
+		// Follow symlinks, but only for paths added with Add(), and not paths
+		// we're adding from internalWatch from a listdir.
+		if !listDir && fi.Mode()&os.ModeSymlink == os.ModeSymlink {
 			link, err := os.Readlink(name)
 			if err != nil {
-				// Return nil because Linux can add unresolvable symlinks to the
-				// watch list without problems, so maintain consistency with
-				// that. There will be no file events for broken symlinks.
-				// TODO: more specific check; returns os.PathError; ENOENT?
-				return "", nil
+				return "", err
+			}
+			if !filepath.IsAbs(link) {
+				link = filepath.Join(filepath.Dir(name), link)
 			}
 
 			_, alreadyWatching = w.watches.byPath(link)
@@ -410,7 +410,7 @@ func (w *kqueue) addWatch(name string, flags uint32) (string, error) {
 			name = link
 			fi, err = os.Lstat(name)
 			if err != nil {
-				return "", nil
+				return "", err
 			}
 		}
 
@@ -694,11 +694,11 @@ func (w *kqueue) internalWatch(name string, fi os.FileInfo) (string, error) {
 		// mimic Linux providing delete events for subdirectories, but preserve
 		// the flags used if currently watching subdirectory
 		info, _ := w.watches.byPath(name)
-		return w.addWatch(name, info.dirFlags|unix.NOTE_DELETE|unix.NOTE_RENAME)
+		return w.addWatch(name, info.dirFlags|unix.NOTE_DELETE|unix.NOTE_RENAME, true)
 	}
 
-	// watch file to mimic Linux inotify
-	return w.addWatch(name, noteAllEvents)
+	// Watch file to mimic Linux inotify.
+	return w.addWatch(name, noteAllEvents, true)
 }
 
 // Register events with the queue.

testdata/watch-dir/create-cyclic-symlink
@@ -9,7 +9,7 @@ echo foo >>/link
 
 Output:
 	write  /link
-	create /link
+	write  /link
 
 	linux, windows, fen:
 		remove  /link

testdata/watch-dir/create-file-in-subdir
@@ -1,0 +1,10 @@
+watch /
+mkdir /dir
+touch /dir/file
+
+Output:
+	create  /dir
+
+	fen: # TODO
+		create  /dir
+		write   /dir

testdata/watch-dir/symlink-dir
@@ -5,8 +5,27 @@ mkdir /dir
 watch /
 ln -s /dir /link
 
+# Shouldn't give any events.
+touch link/file
+
 Output:
 	create  /link
+
+	# kqueue sends an event because it sends an event every time a directory
+	# changes, but fsnotify doesn't detect the symlink as a "directory". It's
+	# been like this for over a decade. When it was changed before people
+	# complained so I'm hesitant to fix it now as part of a bugfix.
+	#
+	# TODO: it shouldn't do this; it doesn't work like this on inotify.
+	kqueue:
+		create  /link
+		write   /link
+
+	# TODO: should also fix FEN, which seems to send a write for regular dirs as
+	# well.
+	fen:
+		create  /link
+		write   /dir
 
 	dragonfly: # TODO: can we fix this?
 		no-events

testdata/watch-symlink/to-dir-relative
@@ -9,6 +9,3 @@ touch /dir/file
 
 Output:
 	create    /link/file
-
-	kqueue:  # TODO: broken
-		no-events

testdata/watch-symlink/to-file-relative
@@ -10,7 +10,5 @@ echo hello >>/file
 Output:
 	write    /link
 
-	kqueue:  # TODO: broken
-		no-events
 	windows:  # TODO: investigate.
 		no-events

commit 2a32c454a8acbba70a51090751264938c92ba265

kqueue: skip ENOENT entries in watchDirectoryFiles (#727)
watchDirectoryFiles only suppressed EACCES/EPERM from internalWatch.
A dangling symlink in the watched directory caused unix.Open to follow
the link and fail with ENOENT, aborting Watcher.Add for the whole
directory. Treat os.ErrNotExist the same as a permission error: mark
the entry as seen and continue. Regression from #681.

backend_kqueue.go
@@ -578,12 +578,14 @@ func (w *kqueue) watchDirectoryFiles(dirPath string) error {
 
 		cleanPath, err := w.internalWatch(path, fi)
 		if err != nil {
-			// No permission to read the file; that's not a problem: just skip.
-			// But do add it to w.fileExists to prevent it from being picked up
-			// as a "new" file later (it still shows up in the directory
+			// No permission, or the entry resolved to a missing target
+			// (e.g. a dangling symlink): not a problem, just skip. But
+			// do mark it as seen to prevent it from being picked up as
+			// a "new" file later (it still shows up in the directory
 			// listing).
 			switch {
-			case errors.Is(err, unix.EACCES) || errors.Is(err, unix.EPERM):
+			case errors.Is(err, unix.EACCES) || errors.Is(err, unix.EPERM) ||
+				errors.Is(err, os.ErrNotExist):
 				cleanPath = filepath.Clean(path)
 			default:
 				return fmt.Errorf("%q: %w", path, err)

testdata/watch-dir/bug-727
@@ -1,0 +1,19 @@
+# https://github.com/fsnotify/fsnotify/issues/727
+#
+# A dangling symlink already present in the directory must not cause
+# Watcher.Add to fail, and must not prevent sibling files from being
+# watched. Regression on kqueue since #681.
+require symlink
+
+ln   -s /nonexistent /dangling
+touch                /file
+
+watch /
+
+touch /after
+
+Output:
+	create  /after
+
+	dragonfly:  # TODO: should fix this if possible.
+		no-events